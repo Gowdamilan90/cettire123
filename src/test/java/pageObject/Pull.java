af11b9f2122a78fd94c668cd7f0170d50a64c5cf
Milan Gowda <gowdamilan90@gmail.com>
2023-05-17 21:27:34 -0400

    commiting Pull.java

--- src/test/java/pageObject/Pull.java
+++ src/test/java/pageObject/Pull.java
@@ -1,1 +1,12 @@
 // this file is for testing PULL command from CommandPrompt
+//>>>>>>>>>> Using for pushing test from command prompt**********
+//>>>>>>>>>> Using for pushing test from command prompt**********
+//>>>>>>>>>> Using for pushing test from command prompt**********
+//>>>>>>>>>> Using for pushing test from command prompt**********
+//>>>>>>>>>> Using for pushing test from command prompt**********
+//>>>>>>>>>> Using for pushing test from command prompt**********
+//>>>>>>>>>> Using for pushing test from command prompt**********
+
+//>>>>>>>>>> Using for pushing test from command prompt**********
+
+
